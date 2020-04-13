45870310ad7ef34e3f0525ccd0d4b0dc45eab83d
IAMPATTRIXX <dexza53@gmail.com>
2020-04-13 17:16:43 +0700

    better

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = express()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://hotel-booking-project-d6271.web.app/')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
+  return next()
+})
+
+//middleware section
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+//import transactions router V2
+const RouterV2 = require('./route/route')
+app.use(RouterV2)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,  () => {
+    console.log(`server is listening at port: ${PORT}`)
+})
